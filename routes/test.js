5f1d277ff42c2ed2d46115c88a0f35c41e4e618a
mastery-rajashekhar-aluguvelly <109941546+mastery-rajashekhar-aluguvelly@users.noreply.github.com>
2022-09-02 12:51:22 +0530

    fix all-contributors

--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const moment = require('moment');
 const _ = require('lodash');
 const {token} = require('../config/config');
 
@@ -22,6 +23,8 @@ router.get('/top-contributors', async (req, res) => {
                 'Authorization': `Bearer ${token}`
             }
         });
+
+        console.log(commits)
 
 
         const users =  {}
@@ -67,4 +70,104 @@ router.get('/top-contributors', async (req, res) => {
 
 });
 
+router.get('/all-contributors', async (req, res) => {
+    try {
+
+        const repo = req.query.repo;
+        const commitsUrl = `https://api.github.com/repos/masterysystems/${repo}/stats/contributors`;
+
+
+        const commits = await axios.get(commitsUrl,{
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        console.log(commits)
+
+
+        const users =  {}
+
+        const output = [];
+
+        const payload = []
+
+        commits.data.forEach(c =>{
+            const stats  = {
+                a: 0,
+                d:0,
+                c:0
+            }
+            c.weeks.forEach(w =>{
+                const getMonth = moment(w.w * 1000).format('MMM-YY')
+                const getWeek = moment(w.w * 1000).format('MM-DD-YY')
+                const foundMonth = payload.findIndex(m=> m.month === getMonth)
+                if(foundMonth > -1){
+                    const monthData = payload[foundMonth]
+                    const foundWeek = monthData.weeks.findIndex(w => w.name === getWeek)
+                    if(foundWeek >-1){
+                        monthData.weeks[foundWeek].users.push({
+                            name: c.author.login,
+                                a: w.a,
+                            d:w.d,
+                            c:w.c,
+                        })
+                    }else{
+                        monthData.weeks.push({
+                            name: getWeek,
+                            users:[{
+                                name: c.author.login,
+                                a: w.a,
+                                d:w.d,
+                                c:w.c,
+                            }]
+                        })
+                    }
+                }else{
+                    payload.push({
+                    month: getMonth,
+                    weeks:[{
+                        name: getWeek,
+                        users:[{
+                            name: c.author.login,
+                            a: w.a,
+                            d:w.d,
+                            c:w.c,
+                        }]
+                    }]
+
+                    })
+                }
+                stats['a'] += w.a;
+                stats['d'] += w.d;
+                stats['c'] += w.c;
+            })
+
+        })
+
+        return res.status(200).json(payload)
+    }catch (e) {
+        console.log(e)
+        return res.status(500).json(
+            'Oops something went wrong!'
+        )
+    }
+
+});
 module.exports = router;
+
+
+
+// [{
+//     month: 'name',
+//     weeks:[{
+//         name: '',
+//         users:[{
+//             name: '',
+//             a:'',
+//             d:'',
+//             c:'',
+//         }]
+//     }]
+//         ...
+// }]
